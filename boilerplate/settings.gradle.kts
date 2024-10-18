1e2aa537a29a4a73df482f68c8623206c13456f1
sglee <sglee487@gmail.com>
2024-10-18 17:01:53 +0900

    move project directory

--- boilerplate/settings.gradle.kts
+++ boilerplate/settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "kotlin-springboot-hexagonal-boilerplate"
+rootProject.name = "boilerplate"
 include("cmd")
 include("domain")
 include("application")
