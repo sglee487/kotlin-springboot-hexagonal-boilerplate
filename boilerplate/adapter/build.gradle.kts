6dd83251f2793100d4480dcbf8e456cf1d90069f
sglee <sglee487@gmail.com>
2024-11-01 18:51:25 +0900

    dto jackson

--- boilerplate/adapter/build.gradle.kts
+++ boilerplate/adapter/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
@@ -18,6 +19,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.data:spring-data-r2dbc")
+
+    // request body json null handling
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 kotlin {
